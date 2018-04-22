1edb1b03934d3a93dd19bb2e3cbd7eef9808ba4d
Rumob <klimlion@gmail.com>
2018-04-22 15:42:06 +0300

    Добавьте файлы проекта.

---
+++ MathReCo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathReCo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
